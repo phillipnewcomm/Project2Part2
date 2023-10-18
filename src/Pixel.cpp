76f555278ca62fd3a33998430f1bae051d754363
phillipnewcomm <131640649+phillipnewcomm@users.noreply.github.com>
2023-10-18 19:26:40 -0400

    Add files via upload

---
+++ src/Pixel.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Pixel.h"
+using namespace std;
+
+
+
+Pixel::Pixel(unsigned char r, unsigned char g, unsigned char b) {
+    red = r;
+    green = g;
+    blue = b;
+}
+
+const unsigned char Pixel::GetRGB(int x) const {
+    if (x == 0)
+        return red;
+    if (x == 1)
+        return green;
+    if (x == 2)
+        return blue;
+    return 0;
+}
---
+++ src/Pixel.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+class Pixel {
+public:
+    Pixel(unsigned char r, unsigned char g, unsigned char b);
+    const unsigned char GetRGB(int x) const;
+private:
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
