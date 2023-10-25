9eef786a4d3f1ee736daa29cd90e1599cc6e2f33
phillipnewcomm <131640649+phillipnewcomm@users.noreply.github.com>
2023-10-25 19:34:28 -0400

    Update HelperMethods.h

--- src/HelperMethods.h
+++ src/HelperMethods.h
@@ -15,13 +15,23 @@ namespace HelperMethods {
     float NormChar(unsigned char c, unsigned char top, unsigned char bottom);
     Image Multiply(const Image& top, const Image& bottom);
     Image Subtract(const Image& top, const Image& bottom);
-    Image Screen(const Image& top, const Image& bottom, const Image& screen);
+    Image Screen(const Image& top, const Image& bottom);
     Image Overlay(const Image& top, const Image& bottom);
     Image AddRGB(const Image& top, unsigned char red, unsigned char green, unsigned char blue);
     Image Scale(const Image& top, bool red, float x, bool green, float y, bool blue, float z);
     vector<Image> SepRGB(const Image& top);
     Image Combine(const Image& red, const Image& green, const Image& blue);
     Image Rotate180(const Image& top);
+    Image Flip(const Image& top);
+    Image OnlyRed(const Image& top);
+    Image OnlyGreen(const Image& top);
+    Image OnlyBlue(const Image& top);
+    Image AddRed(const Image& top, int value);
+    Image AddGreen(const Image& top, int value);
+    Image AddBlue(const Image& top, int value);
+    Image ScaleRed(const Image& top, int value);
+    Image ScaleGreen(const Image& top, int value);
+    Image ScaleBlue(const Image& top, int value);
     // Image Rotate90(const Image& top);
     Image Quadrant(const Image& first, const Image& second, const Image& third, const Image& fourth);
 }
