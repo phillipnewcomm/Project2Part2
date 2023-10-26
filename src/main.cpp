c970e661842a343f560abb5804fda53952cdc64c
phillipnewcomm <131640649+phillipnewcomm@users.noreply.github.com>
2023-10-25 20:00:19 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,23 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <fstream> // Include this header for file validation
+#include <fstream>
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <tracking_image> <operation1> [arguments1] [operation2] [arguments2] ..." << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <output_filename> <tracking_image> <operation1> [arguments1] [operation2] [arguments2] ..." << std::endl;
+        return 1;
+    }
+
+    // Validate the output filename
+    std::string outputFilename = argv[1];
+    if (outputFilename.empty() || outputFilename.rfind(".tga") != outputFilename.length() - 4) {
+        std::cerr << "Invalid file name." << std::endl;
         return 1;
     }
 
     // Validate the tracking image
-    std::string trackingImagePath = argv[1];
+    std::string trackingImagePath = argv[2];
     std::ifstream trackingFile(trackingImagePath.c_str());
     if (!trackingFile.is_open()) {
         std::cerr << "Could not load: '" << trackingImagePath << "'. Sorry!" << std::endl;
@@ -24,9 +31,8 @@ int main(int argc, char* argv[]) {
     Image trackingImage = HelperMethods::readFile(trackingImagePath);
 
     // Iterate through command-line arguments starting from the third argument
-    for (int i = 2; i < argc; i++) {
+    for (int i = 3; i < argc; i++) {
         std::string operation = argv[i];
-
         // Multiplication
         if (operation == "multiply" && i + 1 < argc) {
             std::string imagePath = argv[i + 1];
@@ -34,28 +40,28 @@ int main(int argc, char* argv[]) {
             trackingImage = HelperMethods::Multiply(trackingImage, image);
             i++;
         }
-            // Subtraction
+        // Subtraction
         else if (operation == "subtract" && i + 1 < argc) {
             std::string imagePath = argv[i + 1];
             Image image = HelperMethods::readFile(imagePath);
             trackingImage = HelperMethods::Subtract(trackingImage, image);
             i++;
         }
-            // Overlay
+        // Overlay
         else if (operation == "overlay" && i + 1 < argc) {
             std::string imagePath = argv[i + 1];
             Image image = HelperMethods::readFile(imagePath);
             trackingImage = HelperMethods::Overlay(trackingImage, image);
             i++;
         }
-            // Screen
+        // Screen
         else if (operation == "screen" && i + 1 < argc) {
             std::string imagePath = argv[i + 1];
             Image image = HelperMethods::readFile(imagePath);
             trackingImage = HelperMethods::Screen(trackingImage, image);  // Corrected order of arguments
             i++;
         }
-            // Combine
+        // Combine
         else if (operation == "combine" && i + 2 < argc) {
             std::string greenPath = argv[i + 1];
             std::string bluePath = argv[i + 2];
@@ -64,72 +70,67 @@ int main(int argc, char* argv[]) {
             trackingImage = HelperMethods::Combine(trackingImage, greenImage, blueImage);
             i += 2;
         }
-            // Flip
+        // Flip
         else if (operation == "flip") {
             trackingImage = HelperMethods::Flip(trackingImage);
         }
-            // Only Red
+        // Only Red
         else if (operation == "onlyred") {
             trackingImage = HelperMethods::OnlyRed(trackingImage);
         }
-            // Only Green
+        // Only Green
         else if (operation == "onlygreen") {
             trackingImage = HelperMethods::OnlyGreen(trackingImage);
         }
-            // Only Blue
+        // Only Blue
         else if (operation == "onlyblue") {
             trackingImage = HelperMethods::OnlyBlue(trackingImage);
         }
-            // Add Red
+        // Add Red
         else if (operation == "addred" && i + 1 < argc) {
             int value = std::stoi(argv[i + 1]);
             trackingImage = HelperMethods::AddRed(trackingImage, value);
             i++;
         }
-            // Add Green
+        // Add Green
         else if (operation == "addgreen" && i + 1 < argc) {
             int value = std::stoi(argv[i + 1]);
             trackingImage = HelperMethods::AddGreen(trackingImage, value);
             i++;
         }
-            // Add Blue
+        // Add Blue
         else if (operation == "addblue" && i + 1 < argc) {
             int value = std::stoi(argv[i + 1]);
             trackingImage = HelperMethods::AddBlue(trackingImage, value);
             i++;
         }
-            // Scale Red
+        // Scale Red
         else if (operation == "scalered" && i + 1 < argc) {
             int value = std::stoi(argv[i + 1]);
             trackingImage = HelperMethods::ScaleRed(trackingImage, value);
             i++;
         }
-            // Scale Green
+        // Scale Green
         else if (operation == "scalegreen" && i + 1 < argc) {
             int value = std::stoi(argv[i + 1]);
             trackingImage = HelperMethods::ScaleGreen(trackingImage, value);
             i++;
         }
-            // Scale Blue
+        // Scale Blue
         else if (operation == "scaleblue" && i + 1 < argc) {
             int value = std::stoi(argv[i + 1]);
             trackingImage = HelperMethods::ScaleBlue(trackingImage, value);
             i++;
         }
-            // Unknown operation
+        // Unknown operation
         else {
             std::cerr << "Unknown operation: " << operation << std::endl;
             return 1;
         }
     }
 
-    // Save the final tracking image to an output file
-    HelperMethods::writeFile("output/tracking_output.tga", trackingImage);
+    // Save the final tracking image to the validated output file
+    HelperMethods::writeFile(outputFilename, trackingImage);
 
     return 0;
 }
-
-
-
-
-
